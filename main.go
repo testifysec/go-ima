84e755ba54f26e9dc3d78b636dcbd52b2f9b068f
cole kennedy <colek42@gmail.com>
2021-10-03 18:56:36 -0500

    hash extend test working

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"bufio"
+	"bytes"
+	"crypto/sha1"
 	"crypto/sha256"
+	"encoding/hex"
 	"errors"
 	"fmt"
 	"io"
@@ -12,6 +15,12 @@ import (
 	"strconv"
 	"strings"
 )
+
+type measurmentLog struct {
+	measurments []measurment
+	file        *os.File
+	aggr        []byte
+}
 
 type measurment struct {
 	pcrBank       int
@@ -25,6 +34,7 @@ type measurment struct {
 const imaFile = "/sys/kernel/security/integrity/ima/ascii_runtime_measurements"
 
 func main() {
+	testHash()
 	fileToCheck := os.Args[1]
 
 	f, err := os.Open(imaFile)
@@ -33,7 +43,11 @@ func main() {
 	}
 	defer f.Close()
 
-	measurments := []measurment{}
+	mLog := measurmentLog{
+		measurments: []measurment{},
+		file:        f,
+		aggr:        []byte{},
+	}
 
 	scanner := bufio.NewScanner(f)
 	for scanner.Scan() {
@@ -45,7 +59,11 @@ func main() {
 			log.Fatalf("Error reading IMA file, %v", err)
 		}
 
-		pcrChecksum := []byte(fields[1])
+		pcrChecksum, err := hex.DecodeString(fields[1])
+		if err != nil {
+			log.Fatalf("Error decoding hex string, %v", err)
+		}
+
 		imaPolicyName := fields[2]
 		algocheck := strings.Split(fields[3], ":")
 		if len(algocheck) < 2 {
@@ -55,7 +73,11 @@ func main() {
 		}
 		algo := algocheck[0]
 
-		checksum := []byte(algocheck[1])
+		checksum, err := hex.DecodeString(algocheck[1])
+		if err != nil {
+			log.Fatalf("Error decoding hex string, %v", err)
+		}
+
 		fileName := fields[4]
 
 		newMeasurment := measurment{
@@ -66,18 +88,21 @@ func main() {
 			fileChecksum:  checksum,
 			fileName:      fileName,
 		}
-		measurments = append(measurments, newMeasurment)
+		mLog.measurments = append(mLog.measurments, newMeasurment)
 	}
+
+	mLog.calcAggr()
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
 
-	if err = validateFile(fileToCheck, measurments); err != nil {
+	if err = validateFile(fileToCheck, mLog.measurments); err != nil {
 		log.Fatalf("Validation Error: %v", err)
 	} else {
 		os.Exit(0)
 	}
+
 }
 
 //check return an eror if validation fails
@@ -107,9 +132,7 @@ func validateFile(fn string, ms []measurment) error {
 	for _, m := range ms {
 		if m.fileName == fileToCheck {
 			inMemory = true
-			disk := fmt.Sprintf("%x", shasum)
-			ima := string(m.fileChecksum)
-			if ima != disk {
+			if !bytes.Equal(m.fileChecksum, shasum) {
 				fmt.Printf("Disk: %x\n", shasum)
 				fmt.Printf("IMA : %s\n", m.fileChecksum)
 				checksumError = true
@@ -125,4 +148,70 @@ func validateFile(fn string, ms []measurment) error {
 		return errors.New("not in memory")
 	}
 	return nil
+}
+
+func (mL *measurmentLog) calcAggr() {
+	runningHash, err := hex.DecodeString("")
+	if err != nil {
+		log.Fatalf("Error decoding HEX String, %v", err)
+	}
+
+	runningHash = []byte{}
+
+	for _, m := range mL.measurments {
+
+		if m.imaPolicyName == "ima-ng" {
+			runningHash, err = hex.DecodeString(fmt.Sprintf("%x%x", runningHash, m.fileChecksum))
+			if err != nil {
+				log.Fatalf("Error decoding HEX String, %v", err)
+			}
+			h := sha1.Sum(runningHash)
+			runningHash = h[:]
+
+		} else {
+			fmt.Println(m.imaPolicyName)
+		}
+
+	}
+
+	fmt.Printf("%x", runningHash)
+}
+
+func testHash() {
+	//https://elixir.bootlin.com/linux/v5.14.9/source/security/integrity/ima/ima.h
+	//https://stackoverflow.com/questions/10163436/how-can-a-the-extension-of-the-pcr-value-be-replicated-with-e-g-sha1sum
+
+	v := "aaf4c61ddcc5e8a2dabede0f3b482cd9aea9434d"
+	x := "7c211433f02071597741e6ff5a8ea34789abbf43"
+	s := "39955b37b910e57748368b0922fd44fbff72bda5"
+
+	h0, err := hex.DecodeString(s)
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
+
+	h1, err := hex.DecodeString(v)
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
+
+	h2, err := hex.DecodeString(x)
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
+
+	h3 := append(h1, h2...)
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
+
+	h3str := hex.EncodeToString(h3)
+	h4 := sha1.Sum([]byte(h3str))
+
+	fmt.Printf("%x\n", h0)
+	fmt.Printf("%x\n", h1)
+	fmt.Printf("%x\n", h2)
+	fmt.Printf("%x\n", h3)
+	fmt.Printf("%s\n", hex.EncodeToString(h4[:]))
+
 }
